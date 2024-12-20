a25db421bd222e3da017466a8c7b6771afe62d6f
Muhammad Azhari <muhammadazhariii1209@gmail.com>
2024-12-20 11:33:03 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,27 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-
-    <div class="navbar">
-        <div class="logo">NAMA APLIKASI</div>
-        <div class="menu">
-            <a href="#" class="active">Dashboard</a>
-            <a href="#">Cari Laptop</a>
-        </div>
-    </div>
-    <div class="container">
-        <div class="heading">Dashboard</div>
-        <div class="profile-card">
-            <div class="avatar"></div>
-            <div class="info">
-                <div class="username">Admin</div>
+    
+    <div class="db-container">
+        <div class="navbar">
+            <div class="logo">PICKLAP</div>
+            <div class="menu">
+                <a href="#" class="active">Dashboard</a>
+                <a href="#">Manage</a>
             </div>
-            <form action="" method="GET">
-                <button type="submit" name="logout" value="1" class="logout">Logout</button>
-            </form>
+        </div>
+        <div class="container">
+            <div class="heading">Dashboard</div>
+            <div class="profile-card">
+                <div class="avatar"></div>
+                <div class="info">
+                    <div class="username">Admin</div>
+                </div>
+                <form action="" method="GET">
+                    <button type="submit" name="logout" value="1" class="logout">Logout</button>
+                </form>
+            </div>
         </div>
     </div>
     
