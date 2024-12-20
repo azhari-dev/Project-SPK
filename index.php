4afc2eea1f013b53205ef04880f970e066b02e84
Muhammad Azhari <muhammadazhariii1209@gmail.com>
2024-12-20 11:34:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+
 <body>
 
-    <div class="navbar">
-        <div class="logo">NAMA APLIKASI</div>
-        <div class="menu">
-            <a href="#" class="active">Dashboard</a>
-            <a href="#">Cari Laptop</a>
+    <div class="db-container lp">
+        <div class="navbar lp-1">
+            <div class="logo">PICKLAP</div>
         </div>
-    </div>
-    <div class="container">
-        <div class="heading">Dashboard</div>
-        <div class="profile-card">
-            <div class="avatar"></div>
-            <div class="info">
-                <div class="username">Admin</div>
+        <div class="lp-2">
+            <div class="left section">
+                <p>Temukan Laptop Terbaik Untuk Semua Kebutuhanmu</p>
+                <a href="login.php">Masuk</a>
             </div>
-            <form action="" method="GET">
-                <button type="submit" name="logout" value="1" class="logout">Logout</button>
-            </form>
+            <div class="right section">
+                <p>Apakah kamu bingung memilih laptop yang tepat? Jangan khawatir, di <b>PICKLAP</b> kami siap membantu!</p>
+                <p>Kami memberikan rekomendasi laptop terbaik yang disesuaikan dengan berbagai kategori, mulai dari laptop gaming dengan performa tinggi, laptop murah untuk pelajar dan mahasiswa, hingga laptop terbaik untuk bekerja dengan efisiensi maksimal.</p>
+                <p>Di sini, kamu bisa menemukan laptop yang bukan hanya sesuai dengan kebutuhan, tapi juga cocok dengan anggaranmu, jadi kamu bisa membuat keputusan pembelian yang cerdas!</p>
+            </div>
         </div>
     </div>
 
 </body>
-</html>
 
+</html>
